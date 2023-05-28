564b33acde53daceb8ac6ebfbff2bce18a6c054e
kuldeep8454 <108139109+kuldeep8454@users.noreply.github.com>
2023-05-28 10:36:14 +0530

    project done

---
+++ src/components/About.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='text-4xl font-bold inline border-b-4 border-gray-500'>
+          <p>About</p>
+        </div>
+        <p className='text-xl mt-20'>
+          Lorem ipsum 
+          dolor sit amet consectetur 
+          adipisicing elit. Vel, optio. 
+          Dicta ipsam atque repellat 
+          officiis! Totam amet eius 
+          sequi, expedita qui numquam 
+          nisi pariatur sit. Debitis sunt 
+          harum doloribus deleniti, temporibus 
+          animi placeat laboriosam distinctio 
+          atque odio dolore sit voluptatibus cum 
+          ipsam eos magnam molestiae quidem sequi veritatis 
+          enim fugiat?
+        </p>
+        <br />
+        <p className='text-xl'>
+          Lorem ipsum dolor 
+          sit amet consectetur, 
+          adipisicing elit. Rem, 
+          nobis ipsa. Iure aliquam 
+          deserunt hic veniam laudantium 
+          animi excepturi nam explicabo id 
+          doloribus sequi quos, est possimus 
+          similique temporibus exercitationem.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
+
+// import React from 'react';
+
+// const About = () =>{
+//   return (
+//     <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+
+//       <div className='max-w-scree-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+//         <div className='text-4xl font-bold inline boder-b-4 border-gray-500'>
+//           <p>About</p>
+//         </div>
+//         <p className='text-xl mt-20'>
+//           Lorem ipsum 
+//           dolor sit amet consectetur 
+//           adipisicing elit. Vel, optio. 
+//           Dicta ipsam atque repellat 
+//           officiis! Totam amet eius 
+//           sequi, expedita qui numquam 
+//           nisi pariatur sit. Debitis sunt 
+//           harum doloribus deleniti, temporibus 
+//           animi placeat laboriosam distinctio 
+//           atque odio dolore sit voluptatibus cum 
+//           ipsam eos magnam molestiae quidem sequi veritatis 
+//           enim fugiat?
+//           </p>
+//           <br/>
+//           <p className='text-xl'>
+//             Lorem ipsum dolor 
+//             sit amet consectetur, 
+//             adipisicing elit. Rem, 
+//             nobis ipsa. Iure aliquam 
+//             deserunt hic veniam laudantium 
+//             animi excepturi nam explicabo id 
+//             doloribus sequi quos, est possimus 
+//             similique temporibus exercitationem.
+//           </p>
+//       </div>
+//     </div>
+//   )
+// }
+// export default About;
+
